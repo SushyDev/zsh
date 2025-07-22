f1faf494abf5b60f65cd2fd23bd11bc3abea1b16
SushyDev <xsushy@hotmail.com>
2025-07-22 09:15:00 +0200

    updates

--- custom/themes/monochromatic.zsh
+++ custom/themes/monochromatic.zsh
@@ -1,18 +1,24 @@
-local user_symbol='%(!.#.$)'
-local reset_color="%F{255}%"
-local return_code="%F{240}%(?..%? ↵%{$reset_color})"
-local user_host="%F{255}%n@%m%{$reset_color}"
-local current_dir="%F{245}%~%{$reset_color}"
+local white="%F{255}%"
+local gray="%F{245}%"
+local dark_gray="%F{240}%"
+
+local reset_color="${gray}"
+local command_color="${white}"
+
+local user_symbol="%{$white}%(!.#.$)%{$reset_color}"
+local return_code="%{$dark_gray}%(?..%? ↵%{$reset_color})"
+local user_host="${gray}n@%m${reset_color}"
+local current_dir="${white}~${reset_color}"
 
 precmd() {
-    PROMPT="%{${reset_color}}╭${user_host} ${current_dir} $(git_prompt_info)
-╰${user_symbol} "
+    PROMPT="%{$reset_color}╭${user_host}  ${current_dir}  $(git_prompt_info)
+%{$reset_color}╰${user_symbol}%{$command_color} "
 }
 
 RPROMPT="${return_code}"
 
 ZSH_THEME_GIT_PROMPT_PREFIX=""
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color}"
-ZSH_THEME_GIT_PROMPT_DIRTY="%F{240}"
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color}\e[24m"
+ZSH_THEME_GIT_PROMPT_DIRTY="%F{255}\e[4m"
 ZSH_THEME_GIT_PROMPT_CLEAN="%F{255}"
 
