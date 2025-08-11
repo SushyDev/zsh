f2d1a610704dfb9c2e33d7ff605f5aeb52658863
SushyDev <xsushy@hotmail.com>
2025-08-11 15:25:21 +0200

    cleanup

--- custom/modules/!nix.zsh
+++ custom/modules/!nix.zsh
@@ -1,27 +1,3 @@
-opencode() {
-	nix run nixpkgs#opencode -- "$@"
-}
-
-tmux() {
-	nix run nixpkgs#tmux -- "$@"
-}
-
-fzf() {
-	nix run nixpkgs#fzf -- "$@"
-}
-
-rg() {
-	nix run nixpkgs#ripgrep -- "$@"
-}
-
-nvim() {
-	nix run nixpkgs#neovim -- "$@"
-}
-
-glab() {
-	nix run nixpkgs#glab -- "$@"
-}
-
 local nix_add() {
 	PATH="$(nix-build --no-out-link '<nixpkgs>' -A $1)/bin:$PATH"
 }
