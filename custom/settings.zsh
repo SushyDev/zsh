8fc7f3f1dea1dc3f4676cb8b4f0a1a7900cd8c52
SushyDev <mail@sushy.dev>
2025-11-21 20:39:06 +0100

    system specific settings

--- custom/settings.zsh
+++ custom/settings.zsh
@@ -1,5 +1,18 @@
 export EDITOR=nvim
 
 alias vim="nvim"
-alias x86="arch -x86_64 zsh"
 alias reload="source $ZDOTDIR/.zshrc"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+	alias x86="arch -x86_64 zsh"
+fi
+
+if [[ "$(uname)" == "Linux" ]]; then
+	bindkey '^[[1;5D' backward-word
+	bindkey '^[[1;5C' forward-word
+
+	alias pbcopy='wl-copy'
+	alias pbpaste='wl-paste'
+
+	alias open='xdg-open'
+fi
